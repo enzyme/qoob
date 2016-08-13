226d03a22643e10dfbeff82c63044ac430378dd9
Tristan Strathearn <r3oath@gmail.com>
2016-08-13 19:41:46 +1000

    Add base skeleton implementation, depedencies etc.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var babel = require("gulp-babel");
+
+gulp.task("build", function (cb) {
+    pump([
+        gulp.src("src/qoob.js"),
+        babel(),
+        gulp.dest("build")
+    ], cb);
+});
+
+gulp.task('compress', ['build'], function (cb) {
+    pump(
+        [
+            gulp.src('build/*.js'),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
+});
+
+gulp.task('default', ['build', 'compress']);
---
+++ src/qoob.js
@@ -0,0 +1,29 @@
+(function(name, definition) {
+    if (typeof module != 'undefined') module.exports = definition();
+    else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
+    else this[name] = definition();
+}('qoob', function() {
+    return class Qoob {
+        //
+        constructor(target) {
+            if (target !== null && typeof target === 'object') {
+                this.target = target;
+            } else if (target !== null && typeof target === 'string') {
+                this.target = document.querySelectorAll(target);
+            } else {
+                this.target = null;
+            }
+
+            return this;
+        }
+
+        //
+        first() {
+            if (this.target !== null && this.target.length > 0) {
+                return new Qoob(this.target[0]);
+            }
+
+            return null;
+        }
+    }
+}));
