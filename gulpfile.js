2bd9df50ecfddf4def6bec829bbacb909d045495
Tristan Strathearn <r3oath@gmail.com>
2016-09-11 17:31:59 +1000

    Update build script, expose jsnext and create development and production dist's during compile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
-var gulp = require('gulp');
+var gulp   = require('gulp');
+var pump   = require('pump');
 var uglify = require('gulp-uglify');
-var pump = require('pump');
-var babel = require("gulp-babel");
+var rename = require("gulp-rename");
+var rollup = require('rollup').rollup;
+var rollupBabel = require('rollup-plugin-babel');
 
 gulp.task("build", function (cb) {
-    pump([
-        gulp.src("src/qoob.js"),
-        babel(),
-        gulp.dest("build")
-    ], cb);
+    return rollup({
+        entry: './src/qoob.js',
+        plugins: [
+            rollupBabel()
+        ],
+    }).then(function(bundle) {
+        return bundle.write({
+            format: 'umd',
+            dest: './dist/qoob.js',
+            moduleName: 'Qoob',
+        });
+    });
 });
 
 gulp.task('compress', ['build'], function (cb) {
     pump([
-        gulp.src('build/*.js'),
+        gulp.src('dist/qoob.js'),
         uglify(),
+        rename('qoob.min.js'),
         gulp.dest('dist')
     ], cb);
 });
